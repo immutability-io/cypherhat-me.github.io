75bfe78a4612cec733ba9bf73584a673c78ea48b
cypherhat <jeff@immutability.io>
2021-02-07 18:34:27 -0500

    setup

--- test.sh
+++ test.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-V1=( "take a perfect beer" "now add perfect day" "murder hornet joins?" )
-V2=( "add a lemon? ruin" "if fly's fate spoils your moment?" "I'm reaching for glasses and" )
+V1=( "take a perfect beer" "add a perfect day" "murder hornet joins?" )
+
+V2=( "add a lemon? it's ruined" "if fly's fate spoils your moment?" "I'm reaching for glasses and" )
+
 V3=( "tip the glass over" "the dude can abide" "hoping for lemons" )
 
-
 x=0
+
 echo "✦ a haiku ✦"
+
 while [ $x -le 2 ]
+
 do
+
   echo ""
+
   echo "✦✦✦✦✦ ${V1[$x]}"
+
   echo "✦✦✦✦✦✦✦ ${V2[$x]}"
+
   echo "✦✦✦✦✦ ${V3[$x]}"
+
   x=$(( $x + 1 ))
+
 done
