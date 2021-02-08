951cee6e24d3e85ec97507c5b41f1f2419ae80c2
cypherhat <jeff@immutability.io>
2021-02-08 09:15:08 -0500

    setup

--- lemons.sh
+++ lemons.sh
@@ -5,9 +5,9 @@ V2=( "slice of lemon? it's ruined" "if a fly spoils the moment?" "I'm reaching f
 V3=( "tip the glass over" "no problem at all" "hoping for lemons" )
 
 x=0
-echo "      ✦✦✦✦✦✦✦✦"
 echo "✦✦✦✦✦ a senryū ✦✦✦✦✦"
 echo "      ✦✦✦✦✦✦✦✦"
+echo "        ✦✦✦✦"
 while [ $x -le 2 ]
 do
   echo ""
