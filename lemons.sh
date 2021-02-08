fd975986ba58eb53f4c8d55e4639fdff54911abb
cypherhat <jeff@immutability.io>
2021-02-08 08:26:24 -0500

    setup

--- lemons.sh
+++ lemons.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
 V1=( "take a perfect beer" "add a perfect day" "murder hornet joins?" )
-
 V2=( "slice of lemon? it's ruined" "if a fly spoils the moment?" "I'm reaching for glasses and" )
-
 V3=( "tip the glass over" "no problem at all" "hoping for lemons" )
 
 x=0
 
-echo "✦ a haiku ✦"
+echo "✦ a senryū ✦"
 
 while [ $x -le 2 ]
-
 do
-
   echo ""
-
   echo "✦✦✦✦✦ ${V1[$x]}"
-
   echo "✦✦✦✦✦✦✦ ${V2[$x]}"
-
   echo "✦✦✦✦✦ ${V3[$x]}"
-
   x=$(( $x + 1 ))
-
 done
